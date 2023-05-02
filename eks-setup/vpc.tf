b3c411c4d2b9f1cf14e351fde315c03945e2e957
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-05-02 23:57:13 +0530

    Create vpc.tf

---
+++ eks-setup/vpc.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
