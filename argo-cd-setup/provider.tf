c07aa84947a07163b981dfdfc26f8043718eccf4
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-05-03 01:08:16 +0530

    Update provider.tf

--- argo-cd-setup/provider.tf
+++ argo-cd-setup/provider.tf
@@ -1,12 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
 }
-
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
     }
   }
 }
